8915b9b1368834c01d70844663ee740497386190
michellemtchai <michelle.chai@mail.utoronto.ca>
2017-06-05 11:22:36 -0400

    Move chass importer to be a service

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,10 @@ module MyTAPP
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+
+    # Custom folders autoload
+    config.autoload_paths << "#{Rails.root}/app/services"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
