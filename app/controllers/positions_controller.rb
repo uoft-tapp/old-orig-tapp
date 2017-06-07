812ad05e3a3d94fffd39b4a11d9644d572467b58
michellemtchai <michelle.chai@mail.utoronto.ca>
2017-06-07 16:34:36 -0400

    added GET /courses route

---
+++ app/controllers/positions_controller.rb
@@ -0,0 +1,22 @@
+class PositionsController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def index
+    condition = {course_code: params[:course_code].upcase}
+    @course_positions = Position.where(condition)
+    render json: @course_positions.to_json()
+  end
+
+  def update
+    position = Position.find_by!(id: params[:id],
+      course_code: params[:course_code].upcase)
+    position.update_attributes!(position_params)
+  end
+
+  private
+  def position_params
+    params.permit(:duties, :qualifications, :hours, :estimated_count,
+      :estimated_total_hours)
+  end
+
+end
