7d72b4a8ee775b4f4d0a8095c03ac390dfce837f
michellemtchai <michelle.chai@mail.utoronto.ca>
2017-06-08 11:36:54 -0400

    use Rails API to do /courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,11 +1,6 @@
 class CoursesController < ApplicationController
-  def list
-    column_names = ["title AS course_id", "courses.course_name AS course_name",
-      "'Summer 2017' AS semester", "courses.campus_code AS campus",
-      "estimated_count AS positions", "courses.instructor_id AS instructor",
-      "hours AS hr_position", "1000 AS enrollment", "qualifications",
-      "duties"]
-    join_course = "INNER JOIN courses on positions.course_code=courses.code"
-    render json: Position.joins(join_course).select(column_names)
+  def index
+    @courses = Course.all.includes(:instructor, :positions)
+    render json: @courses.to_json(include: [:instructor, :positions])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/hello_react", to: "hello_react#index"
-  get "/courses", to: "courses#list"
+  resources :courses
 end
