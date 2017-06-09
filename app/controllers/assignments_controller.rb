f7fa573509c05814fab03094571c1460851b8855
simpslandyy <simpslandyy@gmail.com>
2017-06-09 16:19:27 -0400

    New migration -> added columns to assignments model, and scripted index, show and create methods

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,14 +1,40 @@
 class AssignmentsController < ApplicationController
-  def index
 
+ ## TD: check if this all works, then create a few records in the assignments table
+ ## - lastly implement destroy 
+
+  def index
+    # send applicants that have been assigned
+    @assignments = if params[:applicant_id].present? do
+      Assignment.includes(:applicants).where(applicant_id: params[:applicant_id])
+    else
+      Assignment.includes(:applicants).all
+    end
+    render json: @assignments.to_json(include: [:applicants])
   end
 
   def show
+    # Get all the assignments based off applicant_id and/or ID, include applicants
+    @assignment = if params[:applicant_id].present? do
+      Assignments.includes(:applicants).where(id: params[:id], applicant_id: params[:applicant_id]).take!
+    else
+      Assignment.includes(:applicants).find(params[:id])
+    end
+    render json: @assignment.to_json(include: [:applicants])
+  end
 
+  def create
+    # Create an assignment
+    @applicant = Applicant.find(params[:applicant_id])
+    @applicant.assignment.create!(assignment_params)
   end
 
   def destroy
-
   end
-  
+
+  private
+    def assignment_params
+      params.require(:assignment).permit(:round_id, :hours)
+    end
+
 end
