40674640c687989175ab6a3978c613a45713fc30
michellemtchai <michelle.chai@mail.utoronto.ca>
2017-06-09 10:34:24 -0400

    add show course listing by course code

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,11 @@
 class CoursesController < ApplicationController
   def index
-    @courses = Course.all.includes(:instructor, :positions)
+    if params[:code] == nil
+      @courses = Course.all.includes(:instructor, :positions)
+    else
+      condition = {code: params[:code].upcase}
+      @courses = Course.where(condition).includes(:instructor, :positions)
+    end
     render json: @courses.to_json(include: [:instructor, :positions])
   end
 end
