a429e1e690e6ebc5acf81b8c6de00165a4409598
michellemtchai <michelle.chai@mail.utoronto.ca>
2017-06-12 11:16:36 -0400

    commit NoMethodError part

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,33 +1,18 @@
 class CoursesController < ApplicationController
   protect_from_forgery with: :null_session
   def index
-    if params[:code] == nil
-      @courses = Course.all.includes(:instructor, :positions)
-    else
-      condition = {code: params[:code].upcase}
-      @courses = Course.where(condition).includes(:instructor, :positions)
-    end
+    @courses = Course.all.includes(:instructor, :positions)
     render json: @courses.to_json(include: [:instructor, :positions])
   end
 
   def update
-    condition = {id: params[:id]}
-    if Position.exists?(condition)
-      course_code = Position.where(condition).pluck(:course_code)
-
-      get_course_condition = {code: course_code}
-      course_data = {estimated_enrolment: params[:estimated_enrolment]}
-      update_table(Course, get_course_condition, course_data)
-    end
+    course = Course.find(params[:code].upcase)
+    params[:course] = course
+    course.update_attributes!(course_params)
   end
 
   private
-  def update_table(table, condition, data)
-    row = table.where(condition)
-    puts row.to_json()
-    row.update(data)
-    puts row.to_json()
+  def course_params
+    params.require(:course).permit(:estimated_enrolment)
   end
-
-#    params.require(:course).permit()
 end
