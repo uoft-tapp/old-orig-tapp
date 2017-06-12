d66fb2d026e52f45e20cf4c3680ccb35d6bf2d7a
michellemtchai <michelle.chai@mail.utoronto.ca>
2017-06-12 11:58:55 -0400

    finish #update for both courses/positions_controller

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -8,6 +8,7 @@ class PositionsController < ApplicationController
   end
 
   def update
+<<<<<<< 222a05a91c4015d3d09930508f9bf167e4f92dab
     position = Position.find_by!(id: params[:id],
       course_code: params[:course_code].upcase)
     position.update_attributes!(position_params)
@@ -17,6 +18,17 @@ class PositionsController < ApplicationController
   def position_params
     params.permit(:duties, :qualifications, :hours, :estimated_count,
       :estimated_total_hours)
+=======
+    position = Position.find(params[:id])
+    position.update_attributes!(position_params)
+>>>>>>> finish #update for both courses/positions_controller
   end
+
+  private
+  def position_params
+    params.permit(:duties, :qualifications, :hours, :estimated_count,
+      :estimated_total_hours)
+  end
+
 
 end
