733eb8e8e7b2777295d51da857f5b17a8f5a9d68
simpslandyy <simpslandyy@gmail.com>
2017-06-13 14:01:24 -0400

    Implemented methods for assignment endpoint

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,40 +1,93 @@
 class AssignmentsController < ApplicationController
+  protect_from_forgery with: :null_session
 
- ## TD: check if this all works, then create a few records in the assignments table
- ## - lastly implement destroy 
-
+ '''
+    index #GET
+      /assignments - returns a list of assignments including their applicant data
+      /applicants/:applicant_id/assignments - returns an assignments given an applicant ID
+ '''
   def index
-    # send applicants that have been assigned
-    @assignments = if params[:applicant_id].present? do
-      Assignment.includes(:applicants).where(applicant_id: params[:applicant_id])
-    else
-      Assignment.includes(:applicants).all
-    end
-    render json: @assignments.to_json(include: [:applicants])
+    @assignments = if params[:applicant_id].present?
+        Assignment.includes(:applicant).where(applicant_id: params[:applicant_id])
+      else
+        Assignment.includes(:applicant).all
+      end
+
+    render json: @assignments.to_json(include: [:applicant])
   end
 
+  '''
+    show #GET
+      /assignments/:id - returns an assignment given an assignment ID
+      /applicants/:applicant_id/assignments/:id - returns an assignment given an ID, for a given applicant ID
+  '''
   def show
     # Get all the assignments based off applicant_id and/or ID, include applicants
-    @assignment = if params[:applicant_id].present? do
-      Assignments.includes(:applicants).where(id: params[:id], applicant_id: params[:applicant_id]).take!
-    else
-      Assignment.includes(:applicants).find(params[:id])
-    end
-    render json: @assignment.to_json(include: [:applicants])
+    @assignments = if params[:applicant_id].present?
+        Assignment.includes(:applicant).where(id: params[:id], applicant_id: params[:applicant_id]).take!
+      else
+        Assignment.includes(:applicant).find(params[:id])
+      end
+
+    render json: @assignments.to_json(include: [:applicant])
   end
 
+  '''
+    create #POST
+      /applicants/:applicant_id/assignments
+      round_id, hours, position_id
+
+      creates an assignment between an applicant and a position.
+      returns status 209 if tries to create a duplicate
+  '''
   def create
-    # Create an assignment
-    @applicant = Applicant.find(params[:applicant_id])
-    @applicant.assignment.create!(assignment_params)
+    has_assignment = Assignment.where(round_id: params[:round_id], position_id: params[:position_id]).exists?
+
+    unless has_assignment
+      @applicant = Applicant.find(params[:applicant_id])
+      @applicant.assignments.create!(assignment_params)
+
+      render json: @applicant.to_json(include: [:assignments])
+    else
+      render json: { status: 209 } # conflict
+    end
   end
 
-  def destroy
+  '''
+    update #PATCH
+    /applicants/:applicant_id/assignment/:id
+    hours
+
+    updates the hours column for an applicant given assignment ID and applicant ID
+  '''
+  def update
+    @applicant = Applicant.find(params[:applicant_id])
+    @assignment = @applicant.assignments.find(params[:id])
+
+    @assignment.update!(hours: params[:hours])
+
+    render json: @assignment.to_json(include: [:applicant])
   end
+
+
+  '''
+    destroy #DELETE
+    /applicants/:applicant_id/assignment/:id
+
+    removes (unassigns) an assignment record give an applicant ID and assignment ID
+  '''
+ # Unassign an assignee using applicant_id and assignment ID
+  def destroy
+    @applicant = Applicant.find(params[:applicant_id])
+    @assignment = @applicant.assignments.find(params[:id])
+
+    @assignment.destroy!
+  end
+
+
 
   private
     def assignment_params
-      params.require(:assignment).permit(:round_id, :hours)
+      params.permit(:position_id, :round_id, :hours)
     end
-
 end
