74564bedea64721911ac36267c47548cf25ce21e
Gabrielle Singh Cadieux <gabrielle.singhcadieux@mail.utoronto.ca>
2017-06-13 14:37:41 -0400

    initial pieces of the ABC view - problems with importing React and ReactDOM

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/hello_react", to: "hello_react#index"
-
   # /applicants/:applicant_id/applications/:id
   resources :applicants do
     resources :applications
   end
 
   resources :applications, only: [:index, :show]
+  
+  @home = "hello_react"
+  
+  get "/#{@home}", to: "#{@home}#index"
+  get "/#{@home}#courses", to: "#{@home}#courses"
+  get "/#{@home}#applicantsbycourse", to: "#{@home}#applicantsbycourse"
+  get "/#{@home}#assigned", to: "#{@home}#assigned"
+  get "/#{@home}#unassigned", to: "#{@home}#unassigned"
+  get "/#{@home}#summary", to: "#{@home}#summary"
+
+  get "/#{@home}#applicant:id", to: "#{@home}#applicant"
 end
