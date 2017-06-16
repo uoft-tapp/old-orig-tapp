2f81e704740efd8af08e611ba893cedb435ce36f
Gabrielle Singh Cadieux <gabrielle.singhcadieux@mail.utoronto.ca>
2017-06-16 16:59:56 -0400

    turned app state object into a Backbone model; set top-level App component to listen for changes in model and update its own state based on those changes

--- app/javascript/app/components/abc.js
+++ app/javascript/app/components/abc.js
@@ -6,7 +6,7 @@ import { CoursePane } from './coursePane.js'
 class ABC extends React.Component {
     render() {
 	return (
-		<Grid fluid style={{paddingTop: "70px"}}><Row><Col xs={12}>
+		<Grid fluid><Row><Col xs={12}>
 		<CourseMenu {...this.props.courseMenu}/>
 		<CoursePane {...this.props}/>
 		</Col></Row></Grid>
