06128adbe74e9ff54f6b4484cb7f8101d954c112
michellemtchai <michelle.chai@mail.utoronto.ca>
2017-06-16 17:42:07 -0400

    update positions_controller

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -2,20 +2,19 @@ class PositionsController < ApplicationController
   protect_from_forgery with: :null_session
 
   def index
-    @course_positions = Course.find(params[:course_code].upcase).positions
-    render json: @course_positions.to_json()
+    @positions = Position.all.includes(:instructor)
+    render json: @positions.to_json(include: [:instructor])
   end
 
   def update
-    position = Position.find_by!(id: params[:id],
-      course_code: params[:course_code].upcase)
+    position = Position.find(id: params[:id])
     position.update_attributes!(position_params)
   end
 
   private
   def position_params
     params.permit(:duties, :qualifications, :hours, :estimated_count,
-      :estimated_total_hours)
+      :estimated_total_hours, :open, :estimated_enrolment)
   end
 
 end
