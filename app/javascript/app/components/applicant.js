ab5fe80f1e29043a7f0acc2fd4368f69cf5229c5
Gabrielle Singh Cadieux <gabrielle.singhcadieux@mail.utoronto.ca>
2017-06-21 22:23:07 -0400

    implemented more components of ABC view

--- app/javascript/app/components/applicant.js
+++ app/javascript/app/components/applicant.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
-const Applicant = props => {
+class Applicant extends React.Component {
+    selectThisTab() {
+	this.props.nav.selectTab(this.props.nav.applicant.key);
+    }
+    
+    componentDidMount() {
+	this.selectThisTab();
+    }
+
+    componentDidUpdate() {	
+	this.selectThisTab();
+    }
+
+    render() {
 	return <div className="container-fluid" style={{paddingTop: "70px"}}><h1>Applicant {props.match.params.id}!</h1></div>
-};
+    }
+}
 
 export { Applicant };
--- app/javascript/app/components/summary.js
+++ app/javascript/app/components/summary.js
@@ -1,5 +1,21 @@
 import React from 'react'
 
-const Summary = props => <div className="container-fluid" style={{paddingTop: "70px"}}><h1>Summary!</h1></div>;
+class Summary extends React.Component {
+    render() {
+	return <div className="container-fluid" style={{paddingTop: "70px"}}><h1>Summary!</h1></div>;
+    }
+
+    selectThisTab() {
+	this.props.nav.selectTab(this.props.nav.summary.key);
+    }
+    
+    componentDidMount() {
+	this.selectThisTab();
+    }
+
+    componentDidUpdate() {	
+	this.selectThisTab();
+    }
+}
 
 export { Summary };
