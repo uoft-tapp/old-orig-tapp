219af7c9cdb796a99477c368dab67fad4136f651
michellemtchai <michelle.chai@mail.utoronto.ca>
2017-06-23 18:31:40 -0400

    display required panels

--- app/javascript/app/components/assignmentForm.js
+++ app/javascript/app/components/assignmentForm.js
@@ -1,16 +1,26 @@
 import React from 'react'
+import { Panel } from 'react-bootstrap'
 
 const CollapsablePanel = props =>(
-  {props.assignment_panels.map(panel => (
-    <Panel header={panel.label}/>
-  ))}
+  <div className="container-fluid">
+    {props.panels.map(panel => (
+      <Panel style={{width: '98vw', float: 'left'}}
+        header={panel.label}>
+        Test {props.id}
+      </Panel>
+    ))}
+  </div>
 );
 
 class AssignmentForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
-      return (
-        <CollapsablePanel {...this.props}/>
-      );
+    console.log(JSON.stringify(this.props))
+    return (
+      <CollapsablePanel id={this.props.match.params.id} panels={this.props.assignment_form.panels}/>
+    );
   }
 }
 
