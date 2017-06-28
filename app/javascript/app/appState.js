d60b680ba7b3a4b3db035e23642b9598a1db63a9
Gabrielle Singh Cadieux <gabrielle.singhcadieux@mail.utoronto.ca>
2017-06-28 10:45:41 -0400

    removed dependency on React from appState

--- app/javascript/app/appState.js
+++ app/javascript/app/appState.js
@@ -1,11 +1,6 @@
 import _ from 'underscore'
 import Backbone from 'backbone'
 import NestedModel from 'backbone-nested'
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-window.React = React;
-window.ReactDOM = ReactDOM;
 
 const initialState = {
     // navbar component
