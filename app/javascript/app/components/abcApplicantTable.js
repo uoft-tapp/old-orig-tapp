c8d88fa1ca1d9a269629866f335a028b12c382a6
Gabrielle Singh Cadieux <gabrielle.singhcadieux@mail.utoronto.ca>
2017-06-28 12:29:44 -0400

    added component logic for filtering

--- app/javascript/app/components/abcApplicantTable.js
+++ app/javascript/app/components/abcApplicantTable.js
@@ -3,12 +3,12 @@ import { Table } from 'react-bootstrap'
 
 const THeader = props => (
 	<thead><tr>
-	{props.fields.map((field, i) => <th key={'header'+i}>{field}</th>)}
+	{props.fields.map((field, i) => <th key={'header-'+i}>{field}</th>)}
     </tr></thead>
 );
 
 const ApplicantRow = props => (
-	<tr key={props.id}>
+	<tr key={props.id+'-row'}>
 	{props.fields.map(field => (
 		<td key={props.id+field}>
 		{field == 'assigned' ? <input type='checkbox' defaultChecked={props.assigned}/> : props.applicant[field]}
@@ -18,17 +18,17 @@ const ApplicantRow = props => (
 );
 
 class ABCApplicantTable extends React.Component {
-    render() {console.log(this.props.applicants);
+    render() {
 	if (!this.props.applicants.list)
 	    return null;
-	    
+
 	return (
 		<Table striped bordered condensed hover>
 		<THeader fields={this.props.tableHeaders}/>
 		<tbody>
-		{Object.entries(this.props.applicants).map(
+		{Object.entries(this.props.applicants.list).map(
 		    ([key, val]) => (
-			    <ApplicantRow key={'applicant-'+key} applicant={val} id={key}
+			    <ApplicantRow key={'applicant-'+key} applicant={val} id={'applicant-'+key}
 			assigned={this.props.assigned} fields={this.props.tableFields}/>
 		))}
 	    </tbody>
--- app/javascript/app/components/abcTableMenu.js
+++ app/javascript/app/components/abcTableMenu.js
@@ -3,26 +3,32 @@ import { ButtonGroup, Button, DropdownButton, MenuItem, Glyphicon } from 'react-
 
 class ABCTableMenu extends React.Component {
     render() {
+	// style the dropdown if any of its items are active
+	let dropStyle = (fields) => this.props.func.anyFilterActive(this.props.course, fields) ? 'primary' : 'default';
+	
+	let dropSelect = (eventKey) => this.props.func.toggleFilter(this.props.course, eventKey);
+	let isActive = (eventKey) => this.props.func.isFilterActive(this.props.course, eventKey);
+	
 	return (
 		<div style={{marginBottom: '1vh'}}>
 		<ButtonGroup>
-		<Button>Clear filters</Button>
+		<Button onClick={() => this.props.func.clearFilters(this.props.course)}>Clear filters</Button>
 		
-		<DropdownButton title="Status" id="status-dropdown">
-		<MenuItem eventKey="1.1">Assigned elsewhere</MenuItem>
-		<MenuItem eventKey="1.2">Unassigned</MenuItem>
+		<DropdownButton title="Status" id="status-dropdown" bsStyle={dropStyle(['1.1','1.2'])}>
+		<MenuItem eventKey="1.1" onSelect={dropSelect} active={isActive('1.1')}>Assigned elsewhere</MenuItem>
+		<MenuItem eventKey="1.2" onSelect={dropSelect} active={isActive('1.2')}>Unassigned</MenuItem>
 		</DropdownButton>
 
-	    	<DropdownButton title="Prog." id="prog-dropdown">
-		<MenuItem eventKey="2.1">PostDoc</MenuItem>
-		<MenuItem eventKey="2.2">PhD</MenuItem>
-		<MenuItem eventKey="2.3">Masters</MenuItem>
-		<MenuItem eventKey="2.4">UG</MenuItem>
+	    	<DropdownButton title="Prog." id="prog-dropdown" bsStyle={dropStyle(['2.1','2.2','2.3','2.4'])}>
+		<MenuItem eventKey="2.1" onSelect={dropSelect} active={isActive('2.1')}>PostDoc</MenuItem>
+		<MenuItem eventKey="2.2" onSelect={dropSelect} active={isActive('2.2')}>PhD</MenuItem>
+		<MenuItem eventKey="2.3" onSelect={dropSelect} active={isActive('2.3')}>Masters</MenuItem>
+		<MenuItem eventKey="2.4" onSelect={dropSelect} active={isActive('2.4')}>UG</MenuItem>
 		</DropdownButton>
 
-	    	<DropdownButton title="Dept." id="dept-dropdown">
-		<MenuItem eventKey="3.1">DCS</MenuItem>
-		<MenuItem eventKey="3.2">Other</MenuItem>
+	    	<DropdownButton title="Dept." id="dept-dropdown" bsStyle={dropStyle(['3.1','3.2'])}>
+		<MenuItem eventKey="3.1" onSelect={dropSelect} active={isActive('3.1')}>DCS</MenuItem>
+		<MenuItem eventKey="3.2" onSelect={dropSelect} active={isActive('3.2')}>Other</MenuItem>
 		</DropdownButton>
 		
 		</ButtonGroup>
