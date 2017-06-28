b0209dbb876913c238e2242549532e23a61b2732
Gabrielle Singh Cadieux <gabrielle.singhcadieux@mail.utoronto.ca>
2017-06-28 10:43:32 -0400

    added the expected/assigned applicant counts to the course pane header; changed the courseMenu- and coursePane-related state objects to use positionIds as keys instead of course codes

--- app/javascript/app/components/courseMenu.js
+++ app/javascript/app/components/courseMenu.js
@@ -3,23 +3,26 @@ import { ListGroup, ListGroupItem, Panel } from 'react-bootstrap'
 
 class CourseMenu extends React.Component {
     render() {
-	let itemStyle = {height: '2em', padding: '3px'};
+	if (!this.props.courses.list)
+	    return null;
 	
-	const list = this.props.courses.map(
-	    (course) => {
-		return (<ListGroupItem className="course-menu-item" key={course.code} style={itemStyle}
+	let itemStyle = {height: '2em', padding: '3px'};
+
+	const list = Object.entries(this.props.courses.list).map(
+	    ([key, val]) => {
+		return (<ListGroupItem className='course-menu-item' key={'course-' + key} style={itemStyle}
 			onClick={() => {
-			    this.props.toggleSelected(course.code);
-			    this.props.toggleCoursePanel(course.code);
+			    this.props.func.toggleSelectedCourse(key);
+			    this.props.func.toggleCoursePanel(key);
 			}}
-			active={this.props.isSelected(course.code)}>
-			<span style={{float: 'left'}}>{course.code}</span>
-			<span style={{float: 'right'}}>{course.assigned} /{course.expected}</span>
+			active={this.props.func.isCourseSelected(key)}>
+			<span style={{float: 'left'}}>{val.code}</span>
+			<span style={{float: 'right'}}>{val.assignmentCount} /{val.estimatedPositions}</span>
 			</ListGroupItem>);
 	    });
 	
 	return (
-		<Panel header="Courses" style={{height: '100%', overflow: 'auto'}}>
+		<Panel header='Courses' style={{height: '100%', overflow: 'auto'}}>
 		<ListGroup fill>{list}</ListGroup>
 		</Panel>
 	);
