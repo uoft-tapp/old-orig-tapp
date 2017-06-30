9b399858660086b9cb198644e04cbd03874499e1
michellemtchai <michelle.chai@mail.utoronto.ca>
2017-06-30 17:55:18 -0400

    link update attributes for Position to database

--- app/javascript/app/components/courseForm.js
+++ app/javascript/app/components/courseForm.js
@@ -9,7 +9,6 @@ class CourseForm extends React.Component {
   setForms(){
     if(!this.props.func.isFetching()){
       let courses = this.props.courses.list;
-      console.log(courses);
       return(
         Object.entries(courses).map((course, key) =>(
           <ListGroupItem key={key}>
@@ -18,9 +17,9 @@ class CourseForm extends React.Component {
               <tbody>
                 <tr>
                   <td>
-                    <p><input type="text" value={course[1].code} className="course" readonly disabled/></p>
-                    <p><input type="text" value={course[1].name} readonly disabled/></p>
-                    <p><input type="text" value={course[1].campus} readonly disabled/></p>
+                    <p><input type="text" value={course[1].code} className="course" readOnly disabled/></p>
+                    <p><input type="text" value={course[1].name} readOnly disabled/></p>
+                    <p><input type="text" value={course[1].campus} readOnly disabled/></p>
                   </td>
                   <td>
                     <p><b>Positions: </b></p>
@@ -28,21 +27,34 @@ class CourseForm extends React.Component {
                     <p><b>Estimated Enrollment: </b></p>
                   </td>
                   <td>
-                    <p><input type="number" value={course[1].estimatedPositions} min="0"/></p>
-                    <p><input type="number" value={course[1].positionHours} min="0"/></p>
-                    <p><input type="number" value={course[1].estimateEnrol} min="0"/></p>
+                    <p>
+                      <input type="number" value={course[1].estimatedPositions} min="0"
+                        onChange={(eventKey)=>this.props.func.updateCoursePosition(course[0], eventKey)}/>
+                    </p>
+                    <p>
+                      <input type="number" value={course[1].positionHours} min="0"
+                        onChange={(eventKey)=>this.props.func.updateCourseHours(course[0], eventKey)}/>
+                    </p>
+                    <p>
+                      <input type="number" value={course[1].estimatedEnrol?course[1].estimatedEnrol:0} min="0"
+                        onChange={(eventKey)=>this.props.func.updateCourseEnrol(course[0], eventKey)}/>
+                    </p>
                   </td>
                   <td>
                   <p><b>Instructors: </b></p>
-                    <textarea>{JSON.stringify(course[1].instructors)}</textarea>
+                    <textarea value={JSON.stringify(course[1].instructors)} readOnly></textarea>
                   </td>
                   <td>
                     <p><b>Qualifications: </b></p>
-                    <textarea>{course[1].qual}</textarea>
+                    <textarea
+                      onChange={(eventKey)=>this.props.func.updateCourseQual(course[0], eventKey)}
+                      value={course[1].qual}></textarea>
                   </td>
                   <td>
                     <p><b>Responsibilities: </b></p>
-                    <textarea>{course[1].resp}</textarea>
+                    <textarea
+                      onChange={(eventKey)=>this.props.func.updateCourseResp(course[0], eventKey)}
+                      value={course[1].resp}></textarea>
                   </td>
                 </tr>
               </tbody>
