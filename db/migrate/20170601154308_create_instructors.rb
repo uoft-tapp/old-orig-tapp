0fc2e8e6f0b56603e54f7ba3ad0963c75aa2f69e
michellemtchai <michelle.chai@mail.utoronto.ca>
2017-07-06 13:44:33 -0400

    finish importing instructor data

--- db/migrate/20170601154308_create_instructors.rb
+++ db/migrate/20170601154308_create_instructors.rb
@@ -3,6 +3,7 @@ class CreateInstructors < ActiveRecord::Migration[5.1]
     create_table :instructors do |t|
       t.string :name
       t.string :email
+      t.string :utorid
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20170626185602) do
   create_table "instructors", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "utorid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_instructors_on_email", unique: true
