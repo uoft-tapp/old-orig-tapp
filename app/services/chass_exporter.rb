8c3150fd83fe4c4f345978a32ad6d9fbdb142d19
michellemtchai <michelle.chai@mail.utoronto.ca>
2017-07-07 14:06:56 -0400

    create general exporter setup

---
+++ app/services/chass_exporter.rb
@@ -0,0 +1,23 @@
+class ChassExporter
+    def initialize(file)
+      assignments = Assignment.all.includes([:position, :applicant])
+      applicants = Applicant.all.includes([:applications])
+      data=[]
+      assignments.each do |assignment|
+        hours = assignment[:hours]
+        course_id = assignment.position[:position]
+        round_id = assignment.position[:round_id]
+        applications = applicants.find(assignment.applicant[:id]).applications
+        app_id = applications[0][:app_id]
+        data.push({
+          app_id: app_id,
+          course_id: course_id,
+          hours: hours,
+          round_id: round_id.to_s
+        })
+      end
+      File.open("#{Rails.root}/db/#{file}.json", "w") do |file|
+        file.puts "assignments = #{data.to_json}"
+      end
+    end
+end
--- lib/tasks/db.rake
+++ lib/tasks/db.rake
@@ -4,5 +4,10 @@ namespace :db do
       importer = ChassImporter.new("seeds/mock_chass")
       puts "Mock CHASS data import successful!"
     end
+    task export: :environment do
+      file = "seeds/export_data"
+      exporter = ChassExporter.new(file)
+      puts "Assignments have been exported to #{file}"
+    end
   end
 end
