124cb59f90148ba6526e4a8a04be32be4983f58a
michellemtchai <michelle.chai@mail.utoronto.ca>
2017-07-12 12:42:56 -0400

    use CSV generator from rails

--- app/services/csv_generator.rb
+++ app/services/csv_generator.rb
@@ -1,4 +1,5 @@
 class CSVGenerator
+  require 'csv'
 
   def initialize
     @assignments = Assignment.all.includes([:position, :applicant])
@@ -10,8 +11,7 @@ class CSVGenerator
     if @assignments.size == 0
       puts "Warning: You have not made any assignments yet. Operation aborted"
     else
-      data = get_cdf_info
-      attributes = generate_csv_string([
+      attributes = [
         "course_code",
         "email_address",
         "studentnumber",
@@ -19,8 +19,9 @@ class CSVGenerator
         "givenname",
         "student_department",
         "utorid",
-      ])
-      write_export_file("cdf_info", data, attributes)
+      ]
+      data = get_cdf_info(attributes)
+      write_export_file("cdf_info", data)
       puts "Success: CDF info CSV file created"
     end
   end
@@ -29,8 +30,7 @@ class CSVGenerator
     if @assignments.size == 0
       puts "Warning: You have not made any assignments yet. Operation aborted"
     else
-      data = get_offers
-      attributes = generate_csv_string([
+      attributes = [
         "course_code",
         "course_title",
         "offer_hours",
@@ -41,78 +41,77 @@ class CSVGenerator
         "student_department",
         "email_address",
         "round_id",
-      ])
-      write_export_file("offers", data, attributes)
+      ]
+      data = get_offers(attributes)
+      write_export_file("offers", data)
       puts "Success: Offer CSV file created"
     end
   end
 
   def generate_transcript_access
-    data = get_transcript_access
-    attributes = generate_csv_string([
-      "student_number",
-      "familyname",
-      "givenname",
-      "grant",
-      "email_address"
-    ])
-    write_export_file("transcript_access", data, attributes)
-    puts "Suceess: Transcript Access CSV file created"
-  end
-
-  private
-  def write_export_file(file, data, attributes)
-    File.open("#{Rails.root}/db/seeds/#{file}.csv", "w") do |file|
-      file.puts "#{attributes}#{data}"
+    if @applicants.size == 0
+      puts "Warning: There are currenly no applicant in the system. Operation aborted"
+    else
+      attributes = [
+        "student_number",
+        "familyname",
+        "givenname",
+        "grant",
+        "email_address"
+      ]
+      data = get_transcript_access(attributes)
+      write_export_file("transcript_access", data)
+      puts "Suceess: Transcript Access CSV file created"
     end
   end
 
-  def get_cdf_info
-    data =""
-    @assignments.each do |assignment|
-      course = assignment.position
-      applicant = assignment.applicant
-      csv_string = generate_csv_string([
-        course[:position],
-        applicant[:email],
-        applicant[:student_number],
-        applicant[:last_name],
-        applicant[:first_name],
-        applicant[:dept],
-        applicant[:utorid],
-      ])
-      data = (data + csv_string)
+  private
+  def write_export_file(file, data)
+    File.open("#{Rails.root}/db/seeds/#{file}.csv", "w") do |file|
+      file.puts "#{data}"
+    end
+  end
+
+  def get_cdf_info(attributes)
+    data = CSV.generate do |csv|
+      csv << attributes
+      @assignments.each do |assignment|
+        course = assignment.position
+        applicant = assignment.applicant
+        csv << [
+          course[:position],
+          applicant[:email],
+          applicant[:student_number],
+          applicant[:last_name],
+          applicant[:first_name],
+          applicant[:dept],
+          applicant[:utorid],
+        ]
+      end
     end
     return data
   end
 
-  def generate_csv_string(row)
-    csv_string = ""
-    row.each do |item|
-      csv_string = (csv_string + item + ", ")
-    end
-    return (csv_string[0, csv_string.size-2]+"\n")
-  end
-
-  def get_offers
-    data = ""
-    @assignments.each do |assignment|
-      course = assignment.position
-      applicant = assignment.applicant
-      course_code = course[:position]
-      csv_string = generate_csv_string([
-        course[:position],
-        course[:course_name],
-        assignment[:hours].to_s,
-        applicant[:student_number].to_s,
-        applicant[:last_name],
-        applicant[:first_name],
-        get_status(applicant[:program_id]),
-        applicant[:dept],
-        applicant[:email],
-        course[:round_id].to_s,
-      ])
-      data = (data + csv_string)
+  def get_offers(attributes)
+    data = CSV.generate do |csv|
+      csv << attributes
+      @assignments.each do |assignment|
+        course = assignment.position
+        applicant = assignment.applicant
+        course_code = course[:position]
+        csv << [
+          course[:position],
+          course[:course_name],
+          assignment[:hours].to_s,
+          applicant[:student_number].to_s,
+          applicant[:last_name],
+          applicant[:first_name],
+          get_status(applicant[:program_id]),
+          applicant[:dept],
+          applicant[:email],
+          course[:round_id].to_s,
+        ]
+      end
     end
     return data
   end
@@ -134,18 +133,19 @@ class CSVGenerator
     end
   end
 
-  def get_transcript_access
-    data = ""
-    @applicants.each do |applicant|
-      application =  applicant.applications[0]
-      csv_string = generate_csv_string([
-        applicant[:student_number],
-        applicant[:last_name],
-        applicant[:first_name],
-        application[:access_acad_history].downcase,
-        applicant[:email],
-      ])
-      data = (data + csv_string)
+  def get_transcript_access(attributes)
+    data = CSV.generate do |csv|
+      csv << attributes
+      @applicants.each do |applicant|
+        application =  applicant.applications[0]
+        csv << [
+          applicant[:student_number],
+          applicant[:last_name],
+          applicant[:first_name],
+          application[:access_acad_history].downcase,
+          applicant[:email],
+        ]
+      end
     end
     return data
   end
