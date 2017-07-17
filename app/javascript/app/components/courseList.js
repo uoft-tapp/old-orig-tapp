a77a95fc25f6851e2c289feee22df8aa20415b6d
Gabrielle Singh Cadieux <gabrielle.singhcadieux@mail.utoronto.ca>
2017-07-17 11:07:13 -0400

    removed the styling that would unhide overflowing text in the CourseList/CourseMenu; added tooltip text instead

--- app/javascript/app/components/courseList.js
+++ app/javascript/app/components/courseList.js
@@ -2,15 +2,11 @@ import React from 'react';
 import { Panel, ListGroup, ListGroupItem } from 'react-bootstrap';
 
 class CourseList extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     setCourseList() {
         if (!this.props.func.anyFetching()) {
             let courses = this.props.func.getCoursesList();
             return Object.entries(courses).map((course, key) =>
-                <ListGroupItem key={key} href={'#' + course[0]}>
+                <ListGroupItem key={key} href={'#' + course[0]} title={course[1].code}>
                     {course[1].code}
                 </ListGroupItem>
             );
