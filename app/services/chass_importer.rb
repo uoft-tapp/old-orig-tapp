f11ced74271b3f4fe223de139e041a87044d0510
michellemtchai <michelle.chai@mail.utoronto.ca>
2017-08-10 16:13:57 -0400

    change create to create! and update_attributes to update_attributes! (#145)

--- app/services/chass_importer.rb
+++ app/services/chass_importer.rb
@@ -43,7 +43,7 @@ class ChassImporter
 
   def insertion_helper(model, data, ident, exists)
     unless model.where(ident).exists?
-      db_model = model.create(data)
+      db_model = model.create!(data)
       Rails.logger.debug "new #{JSON.pretty_generate(db_model.as_json)}\n\n"
       db_model.save!
       return db_model
@@ -51,7 +51,7 @@ class ChassImporter
       Rails.logger.debug "#{exists}"
       db_model = model.find_by(ident)
       Rails.logger.debug "existing model #{JSON.pretty_generate(db_model.as_json)}\n"
-      db_model.update_attributes(data)
+      db_model.update_attributes!(data)
       Rails.logger.debug "update model #{exists}\nupdate to #{JSON.pretty_generate(db_model.as_json)}\n\n"
       db_model.save!
       return db_model
