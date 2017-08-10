60bd305647bbdf87d07f82e51b0d1e569d071cb1
michellemtchai <michelle.chai@mail.utoronto.ca>
2017-08-10 17:34:16 -0400

    separate export chass routes

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -1,10 +1,21 @@
 class ExportController < ApplicationController
   protect_from_forgery with: :null_session
 
-  def chass
+  def create_chass
     exporter = ChassExporter.new
     response = exporter.export(params[:round_id])
-    render_helper(response)
+    render json: response
+  end
+
+  def download_chass
+    if params[:round_id]
+      file_path = "#{Rails.root}/db/exports/offers_#{params[:round_id]}.json"
+      File.open(file_path, "rb") do |f|
+        @data = f.read
+      end
+      File.delete(file_path)
+      render_helper({generated:true, data: @data, content_type: "application/json", file: "offers_#{params[:round_id]}.json"})
+    end
   end
 
   def cdf
@@ -28,7 +39,7 @@ class ExportController < ApplicationController
   private
   def render_helper(response)
     if response[:generated]
-      send_data response[:data], :filename => response[:file],
+      send_data response[:data], filename: response[:file],
       content_type: response[:type]
     else
       render status: 404, json: {
