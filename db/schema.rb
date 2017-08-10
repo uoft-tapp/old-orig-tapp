6d30d1290a0c76dc29babd5df923b12fdfeb9cfe
michellemtchai <michelle.chai@mail.utoronto.ca>
2017-08-10 12:23:20 -0400

    add full_time to applicant model and fix up mock data and spec

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728235659) do
+ActiveRecord::Schema.define(version: 20170810154932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170728235659) do
     t.string "dept"
     t.integer "yip"
     t.string "program_id"
+    t.string "full_time"
     t.index ["utorid"], name: "index_applicants_on_utorid", unique: true
   end
 
