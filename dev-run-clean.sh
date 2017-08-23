90675bf42140c9d1206b816cf3cdadb800ead7f1
jmzaleski <matz@cs.toronto.edu>
2017-08-23 11:40:44 -0400

    surround stuff with set -x to display the actual commands
    toss db seed which we don't need any more.

--- dev-run-clean.sh
+++ dev-run-clean.sh
@@ -13,17 +13,19 @@ echo 'have you shut down containers already with docker down?'
 echo 'you should consider down -v to remove the postgres volume. hit enter to continue'
 read -p 'hit enter to continue: [interrupt to quit]: '
 
-read -p 'enter to cp dev.env.devfault .env : ' JUNK
+read -p 'hit enter to cp dev.env.devfault .env : ' JUNK
+
+set -x
 cp dev.env.default .env || die failed to copy dev.env.default
+set -
 
 read -p 'enter to `docker rm -f` your containers: ' JUNK
+set -x
 docker-compose rm -f  || die docker-compose rm -f failed
+set -
 
+set -x
 docker-compose up -d --force-recreate || die docker-compose up --force-recreate failed
+set -
 
-#actually all that is needed here is a sleep to wait for postgress to come up.. but how long?
-read -p 'enter to `seed postgres db:' JUNK
-docker-compose run rails-app rake db:migrate db:seed  || die "rake db:migrate db:seed failed"
 
-#read -p 'enter to migrate mock chass data? [interrupt to quit, in which case tapp db will be seeded, but empty]' JUNK
-#docker-compose run rails-app rake db:migrate 'db:seed:chass[mock_chass.json]'
