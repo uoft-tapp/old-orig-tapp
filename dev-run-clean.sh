83dcac612cf760b0ddd55e6438af3730b186cb6d
jmzaleski <matz@cs.toronto.edu>
2017-08-23 12:06:52 -0400

    code copy prod into copy

--- dev-run-clean.sh
+++ dev-run-clean.sh
@@ -7,25 +7,39 @@ die(){
 
 # start up tapp for development 
 
-docker-compose ps
-
 echo 'have you shut down containers already with docker down?'
-echo 'you should consider down -v to remove the postgres volume. hit enter to continue'
-read -p 'hit enter to continue: [interrupt to quit]: '
+echo 'examine running containers, if any in table below'
+docker ps
+
+echo
+echo 'if there are any tapp containers listed above you should consider '
+echo 'docker-compose down, perhaps even down -v (to remove the postgres volume)'
+echo
 
 read -p 'hit enter to cp dev.env.devfault .env : ' JUNK
 
-set -x
+(set -x
 cp dev.env.default .env || die failed to copy dev.env.default
-set -
+)
+
+#TODO: factor following. currently code copy of prod version!
 
 read -p 'enter to `docker rm -f` your containers: ' JUNK
-set -x
-docker-compose rm -f  || die docker-compose rm -f failed
-set -
 
-set -x
+(set -x
+docker-compose rm -f  || die docker-compose rm -f failed
+)
+
+read -p 'enter to `docker-compose up tapp containers: ' JUNK
+
+(set -x
 docker-compose up -d --force-recreate || die docker-compose up --force-recreate failed
-set -
+)
+
+read -p 'enter to `migrate postgres db: ' JUNK
+
+(set -x
+docker-compose run rails-app rake db:migrate  || die "rake db:migrate failed"
+)
 
 
